7bdeaa799b58e2217dd2e086e7d9c961a14c9c57
Martin-Minev <72812000+Martin-Minev@users.noreply.github.com>
2021-02-27 00:49:13 +0100

    1st

---
+++ LinkedList/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node node = new Node(1);
+
+            SoftUniLinkedList linkedList = new SoftUniLinkedList();
+
+            Console.WriteLine("Remove empty head: " + linkedList.RemoveHead());
+
+            for (int i = 0; i < 3; i++)
+            {
+                linkedList.AddHead(new Node(i));
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                linkedList.AddTail(new Node(i));
+            }
+
+            Console.WriteLine("Remove head: " + linkedList.RemoveHead().Value);
+
+            Console.WriteLine("Remove tail: " + linkedList.RemoveTail().Value);
+
+            Node currentNode = linkedList.Tail;
+
+            //while (currentNode != null)
+            //{
+            //    Console.WriteLine(currentNode.Value);
+            //    currentNode = currentNode.Next;
+            //}
+            Console.WriteLine("Foreach from head:");
+            linkedList.ForeachFromHead((node) => { Console.WriteLine($"From action: {node.Value}");});
+
+            int[] linkedListAsArray = linkedList.ToArray();
+        }
+    }
+}
